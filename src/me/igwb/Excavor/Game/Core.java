1970f083888a70bbe83947f61e9144dc3efa4138
Luka <Littlefinix@hotmail.com>
2013-04-27 13:52:43 +0200

    Merge

--- src/me/igwb/Excavor/Game/Core.java
+++ src/me/igwb/Excavor/Game/Core.java
@@ -4,22 +4,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.Point;
 import java.awt.image.BufferStrategy;
-import java.io.IOException;
-import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.annotation.Resource;
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-
-import me.igwb.Excavor.Environment.Field;
-import me.igwb.Excavor.Environment.FieldType;
-import me.mytl.Excavor.Logic.Delay;
-
-import resources.ResourceLoader;
 
 
 public class Core {
@@ -27,29 +14,31 @@ public class Core {
 	private Logger log = Logger.getLogger(Core.class.getName());
 	private MainWindow GameWindow;
 	private MainCanvas GameCanvas;
-	private int FieldSize = 50, GameCanvasSize = 600;
-	int fps = 0;
-	
-	URL texture;
-	
-	GraphicsRenderer gr;
-	
+	private InfoCanvas MainInfo;
+	private Dimension FieldSize = new Dimension(50,50), GameCanvasSize = new Dimension(600,600), InfoCanvasSize = new Dimension(600,80);
+	boolean isRunning = false;
 	
 	protected void initialize() {
 		GameWindow = new MainWindow();
 		GameCanvas = new MainCanvas();
+		MainInfo = new InfoCanvas();
 		
+		GameWindow.setLayout(null);
 		GameWindow.setSize(900,850);
 
+		GameCanvas.setBounds(50, 50, GameCanvasSize.width, GameCanvasSize.height);
+		MainInfo.setBounds(50, 675, InfoCanvasSize.width, InfoCanvasSize.height);
+		
 		
 		GameWindow.add(GameCanvas);
+		GameWindow.add(MainInfo);
 		
-		
+
 		
 		GameWindow.setVisible(true);
 		
 		GameCanvas.createBufferStrategy(2);
-		GameCanvas.setBounds(50, 50, GameCanvasSize, GameCanvasSize);
+		MainInfo.createBufferStrategy(2);
 		
 		run();
 		
@@ -58,75 +47,46 @@ public class Core {
 	private void run() {
 		log.finer("Game loop started");
 		
-		int frames = 0;
-		long totalTime = 0;
-		long curTime = 0;
-		long lastTime = 0;
+		isRunning = true;
 		
-		int i = 0;
-		GameCanvas.setSize(new Dimension(700, 700));
-		
-		int delay = 1000 / 60;
+		while(isRunning) {
 
-		Delay d = new Delay(delay);
-		
-		while(i < 190000000) {
-			lastTime = curTime;
-			curTime = System.currentTimeMillis();
-			
-			if(d.checkDelay()) {
-				d.expand(1000 / 60);
-				
-				GameCanvas.setSize(new Dimension(700, 700));
-				render();
-			}
-			
-			if(totalTime > 1000) {
-				totalTime = 0;
-				fps = frames;
-				frames = 0;
-			}
-			frames ++;			
-			
-			i ++;
+			RenderGame();
+			RenderInfo();
 		}
-		System.exit(0);
+		
 		log.fine("Game loop ended");
 	}
 	
-	private void render() {
+	private void RenderGame() {
 		Graphics g = null;
-		
+
 		try {
 			BufferStrategy buffer = GameCanvas.getBufferStrategy();
 
 			g = buffer.getDrawGraphics();
 
+			
 			g.setColor(Color.WHITE);
+			g.fillRect(0, 0, 600, 600);
 			
-			g.fillRect(0, 0, 1000, 800);
-			
-			g.setColor(Color.RED);
-			
-			g.drawRect(0, 0, GameCanvas.getSize().width, GameCanvas.getSize().height);
 			
 			g.setColor(Color.BLACK);
 			
 			
-			for (int i = 0; i < Math.pow(GameCanvasSize / FieldSize,2); i++) {
-				
-				
-				
-			}
-			
 			g.drawString(System.getProperty("user.dir"), 50, 50);
-			g.drawString("FPS: " + fps, 50, 100);
+			
+			Image img = resources.ResourceLoader.getImage(("/resources/view.png"));
+			
+			
+			g.drawImage(img, 0, 0, null);
 			
 			
 			if(!buffer.contentsLost()) {
 				buffer.show();
 			}
 				
+		
 			GameWindow.repaint();
 			Thread.yield();
 			
@@ -138,4 +98,33 @@ public class Core {
 				g.dispose();
 		}
 	}
+
+	private void RenderInfo() {
+		Graphics g = null;
+
+		try {
+			BufferStrategy buffer = MainInfo.getBufferStrategy();
+
+			g = buffer.getDrawGraphics();
+			
+			g.setColor(Color.GRAY);
+			g.fillRect(0, 0, InfoCanvasSize.width, InfoCanvasSize.height);
+				
+		
+			if(!buffer.contentsLost()) {
+				buffer.show();
+			}
+			
+			GameWindow.repaint();
+			Thread.yield();
+			
+		} catch(Exception e) {
+			log.log(Level.SEVERE, "oh oh", e );
+			
+		} finally {
+			if(g != null)
+				g.dispose();
+		}
+	}
+
 }
