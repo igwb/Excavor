840d4b48768ec37521a5c24350b5e92954defbc4
Luka <Littlefinix@hotmail.com>
2013-05-07 18:21:07 +0200

    Created The Menu Script

---
+++ src/me/igwb/Excavor/Game/SavesManager.java
@@ -0,0 +1,18 @@
+package me.igwb.Excavor.Game;
+
+public class SavesManager {
+
+	private static String lastSave = "";
+	
+	public static void save(String location) {
+		
+		String path = System.getProperty("user.dir") + "/" + file;
+		
+		lastSave = path;
+		
+		String save = 
+		
+		
+	}
+	
+}
