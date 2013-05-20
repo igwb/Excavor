918d41a8457240586ef6ff1bba76f0fc3075f95b
igwb <bodobab@googlemail.com>
2013-05-20 18:10:13 +0200

    Move distance class out of Position.java

---
+++ src/me/igwb/Excavor/Environment/Distance.java
@@ -0,0 +1,45 @@
+package me.igwb.Excavor.Environment;
+
+public class Distance {
+
+	private Integer DistanceX, DistanceY, DistanceZ;
+
+	public Distance(int Dx, int Dy, int Dz) {
+		DistanceX = Dx;
+		DistanceY = Dy;
+		DistanceZ = Dz;
+	}
+
+	public Integer getDistance() {
+		int Dx = Math.abs(DistanceX);
+		int Dy = Math.abs(DistanceY);
+		int Dz = Math.abs(DistanceZ);
+
+		double Output = Math.pow(Dx*Dx + Dy*Dy + Dz*Dz, 0.5); //Root of (Dx² + Dy² + Dz²)
+
+		return Math.round((float)Output);
+	}
+
+	public Integer getDistance(int modX, int modY, int modZ) {
+		int Dx = Math.abs(DistanceX + modX);
+		int Dy = Math.abs(DistanceY + modY);
+		int Dz = Math.abs(DistanceZ + modZ);
+
+		double Output = Math.pow(Dx*Dx + Dy*Dy + Dz*Dz, 0.5); //Root of (Dx² + Dy² + Dz²)
+
+		return Math.round((float)Output);
+	}
+
+	public Integer getDistanceX() {
+		return DistanceX;
+	}
+
+	public Integer getDistanceY() {
+		return DistanceY;
+	}
+
+	public Integer getDistanceZ() {
+		return DistanceZ;
+	}
+
+}
