0117eecc3883202bb94fb4dbf588ee30fc8da72c
igwb <bodobab@googlemail.com>
2013-05-20 18:15:58 +0200

    Move distance class out of Position.java

--- src/me/igwb/Excavor/Environment/Position.java
+++ src/me/igwb/Excavor/Environment/Position.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 import me.igwb.Excavor.Game.Programm;
 
-
-
 public class Position {
 
 	private Integer x, y, z;
@@ -70,49 +68,7 @@ public class Position {
 		return new Distance(DistX, DistY, DistZ);
 	}
 	
-	public class Distance {
-		
-		private Integer DistanceX, DistanceY, DistanceZ;
 
-		public Distance(int Dx, int Dy, int Dz) {
-			DistanceX = Dx;
-			DistanceY = Dy;
-			DistanceZ = Dz;
-		}
-		
-		public Integer getDistance() {
-			int Dx = Math.abs(DistanceX);
-			int Dy = Math.abs(DistanceY);
-			int Dz = Math.abs(DistanceZ);
-			
-			double Output = Math.pow(Dx*Dx + Dy*Dy + Dz*Dz, 0.5); //Root of (Dx² + Dy² + Dz²)
-			
-			return Math.round((float)Output);
-		}
-		
-		public Integer getDistance(int modX, int modY, int modZ) {
-			int Dx = Math.abs(DistanceX + modX);
-			int Dy = Math.abs(DistanceY + modY);
-			int Dz = Math.abs(DistanceZ + modZ);
-			
-			double Output = Math.pow(Dx*Dx + Dy*Dy + Dz*Dz, 0.5); //Root of (Dx² + Dy² + Dz²)
-			
-			return Math.round((float)Output);
-		}
-		
-		public Integer getDistanceX() {
-			return DistanceX;
-		}
-
-		public Integer getDistanceY() {
-			return DistanceY;
-		}
-
-		public Integer getDistanceZ() {
-			return DistanceZ;
-		}
-		
-	}
 	
 	public boolean hasLineOfSight(Position p) {
 		
